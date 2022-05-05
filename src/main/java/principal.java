9edbb6f08ff7f7941b0b7c56f854d237624f6534
Isabel <alum.ipastorl.2021@iesalixar.org>
2022-05-05 19:59:28 +0200

    Initial commit

---
+++ src/main/java/principal.java
@@ -0,0 +1,11 @@
+import com.google.gson.Gson;
+
+public class principal {
+    public static void main(String[] args) {
+        // Serialization
+        BagOfPrimitives obj = new BagOfPrimitives();
+        Gson gson = new Gson();
+        String json = gson.toJson(obj);
+        System.out.println("Hola, Mundo!");
+    }
+}
